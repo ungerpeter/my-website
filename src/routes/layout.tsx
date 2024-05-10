5ee012f8bd8a936ce33207c5425327c61a00229a
Peter Unger <peps.unger@gmail.com>
2024-05-10 21:21:16 +0200

    Implement first draft

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,8 +2,7 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "../components/starter/header/header";
-import Footer from "../components/starter/footer/footer";
+import { LuHome, LuFileText, LuPresentation } from "@qwikest/icons/lucide";
 
 import styles from "./styles.css?inline";
 
@@ -28,11 +27,36 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
+      <aside class="sidebar">
+        <header class="content-wrapper">
+          <div class="avatar-wrapper">
+          <a href="#">
+            <img class="avatar" src="https://avatars.githubusercontent.com/u/4763039?v=4" width={150} height={150} alt="Peter Unger" />
+          </a>
+          </div>
+          <h1>Peter Unger</h1>
+          <h2>Senior Software Engineer</h2>
+        </header>
+        <nav class="content-wrapper">
+          <ul>
+            <li>
+              <a href="#"><LuHome />Home</a>
+            </li>
+            <li>
+              <a href="#"><LuFileText />CV</a>
+            </li>
+            <li>
+              <a href="#"><LuPresentation />Projects</a>
+            </li>
+          </ul>
+        </nav>
+        <footer class="content-wrapper">© 2024 - Peter Unger</footer>
+      </aside>
       <main>
-        <Slot />
+        <div class="container">
+          <Slot />
+        </div>
       </main>
-      <Footer />
     </>
   );
 });
