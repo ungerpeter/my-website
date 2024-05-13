07df18ed0b774995587a164d8a90111d712e469f
Peter Unger <peps.unger@gmail.com>
2024-05-13 14:59:54 +0200

    Basic contact form

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,12 +1,81 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { $, component$ } from "@builder.io/qwik";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import type { InitialValues, SubmitHandler } from "@modular-forms/qwik";
+import { formAction$, useForm, valiForm$ } from "@modular-forms/qwik";
+import { email, type Input, minLength, object, string } from "valibot";
+import { grid } from "~/styled-system/patterns";
+
+export const ContactFormSchema = object({
+  name: string([minLength(1, "Please tell me your name.")]),
+  email: string([
+    minLength(1, "Please tell me your email."),
+    email("The email address seems wrong."),
+  ]),
+  message: string([minLength(1, "Please leave me a message.")]),
+});
+
+export type ContactForm = Input<typeof ContactFormSchema>;
+
+export const useFormLoader = routeLoader$<InitialValues<ContactForm>>(() => ({
+  name: "",
+  email: "",
+  message: "",
+}));
+
+export const useFormAction = formAction$<ContactForm>((values) => {
+  console.log("got form data:", values);
+}, valiForm$(ContactFormSchema));
 
 export default component$(() => {
+  const [, { Form, Field }] = useForm<ContactForm>({
+    loader: useFormLoader(),
+    action: useFormAction(),
+    validate: valiForm$(ContactFormSchema),
+  });
+
+  const handleSubmit = $<SubmitHandler<ContactForm>>((values, event) => {
+    console.log("submitting form", values, event);
+  });
+
   return (
     <>
       <h1>Contact</h1>
       <div class="box">
-        <p>come back later!</p>
+        <Form onSubmit$={handleSubmit} class={grid({})}>
+          <label for="name">Name</label>
+          <Field name="name">
+            {(field, props) => (
+              <div>
+                <input {...props} value={field.value} name="name" />
+                {field.error && <div>{field.error}</div>}
+              </div>
+            )}
+          </Field>
+          <label for="email">E-Mail</label>
+          <Field name="email">
+            {(field, props) => (
+              <div>
+                <input
+                  {...props}
+                  type="email"
+                  value={field.value}
+                  name="email"
+                />
+                {field.error && <div>{field.error}</div>}
+              </div>
+            )}
+          </Field>
+          <label for="message">Message</label>
+          <Field name="message">
+            {(field, props) => (
+              <div>
+                <input {...props} value={field.value} name="message" />
+                {field.error && <div>{field.error}</div>}
+              </div>
+            )}
+          </Field>
+          <button type="submit">Hit me up</button>
+        </Form>
       </div>
     </>
   );
