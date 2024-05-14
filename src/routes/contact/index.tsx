1466b4c24c40401c6e58b1803d5f1708b239c7ec
Peter Unger <peps.unger@gmail.com>
2024-05-14 09:32:29 +0200

    Add buymeacoffee button

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -77,6 +77,15 @@ export default component$(() => {
           <button type="submit">Hit me up</button>
         </Form>
       </div>
+      <br />
+      <a href="https://www.buymeacoffee.com/peterunger" target="_blank">
+        <img
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+          alt="Buy Me A Coffee"
+          height={60}
+          width={217}
+        />
+      </a>
     </>
   );
 });
