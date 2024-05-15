d8bded5267d04e4de891d75acd13e159ed80938d
Peter Unger <peps.unger@gmail.com>
2024-05-15 04:17:01 +0200

    Basic responsive sidebar

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,19 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import { component$, Slot, useSignal, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import { LuHome, LuFileText, LuPresentation, LuNewspaper, LuMessageCircle } from "@qwikest/icons/lucide";
+import {
+  LuHome,
+  LuFileText,
+  LuPresentation,
+  LuNewspaper,
+  LuMessageCircle,
+  LuMenu,
+} from "@qwikest/icons/lucide";
 
 import styles from "./styles.css?inline";
 import { NavLink } from "~/components/nav-link/nav-link";
+import { css } from "~/styled-system/css";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -26,14 +34,64 @@ export const useServerTimeLoader = routeLoader$(() => {
 
 export default component$(() => {
   useStyles$(styles);
+  const sidebarRef = useSignal<HTMLDivElement>();
   return (
     <>
-      <aside class="sidebar">
+      <div
+        class={css({
+          margin: "1rem",
+          color: "var(--fg-color-muted)",
+          backgroundColor: "var(--bg-color-primary)",
+          border: "1px solid var(--border-color)",
+          borderRadius: "0.5rem",
+          padding: "0.5rem",
+          fontSize: "3xl",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          zIndex: 7000,
+          cursor: "pointer",
+          transition: "all 0.16s",
+          _hover: {
+            color: "var(--fg-color)",
+            borderColor: "var(--fg-color)",
+          },
+          md: {
+            display: "none",
+          },
+          "&.open": { zIndex: 100 },
+        })}
+        onClick$={() => {
+          sidebarRef.value?.classList.toggle("open");
+        }}
+      >
+        <LuMenu />
+      </div>
+      <aside
+        ref={sidebarRef}
+        class={[
+          "sidebar",
+          css({
+            zIndex: 5000,
+            transform: "translateX(-100%)",
+            transition: "transform 0.16s cubic-bezier(0.4, 0, 0.2, 1)",
+            userSelect: "none",
+            md: { transform: "translateX(0)" },
+            "&.open": { transform: "translateX(0)" },
+          }),
+        ]}
+      >
         <header class="content-wrapper">
           <div class="avatar-wrapper">
-          <a href="/">
-            <img class="avatar" src="https://avatars.githubusercontent.com/u/4763039?v=4" width={150} height={150} alt="Peter Unger" />
-          </a>
+            <a href="/">
+              <img
+                class="avatar"
+                src="https://avatars.githubusercontent.com/u/4763039?v=4"
+                width={150}
+                height={150}
+                alt="Peter Unger"
+              />
+            </a>
           </div>
           <h1>Peter Unger</h1>
           <h2>Senior Software Engineer</h2>
@@ -41,25 +99,55 @@ export default component$(() => {
         <nav class="content-wrapper">
           <ul>
             <li>
-              <NavLink href="/"><LuHome />About me</NavLink>
+              <NavLink href="/">
+                <LuHome />
+                About me
+              </NavLink>
             </li>
             <li>
-              <NavLink href="/cv/"><LuFileText />My cv</NavLink>
+              <NavLink href="/cv/">
+                <LuFileText />
+                My cv
+              </NavLink>
             </li>
             <li>
-              <NavLink href="/portfolio/"><LuPresentation />My portfolio</NavLink>
+              <NavLink href="/portfolio/">
+                <LuPresentation />
+                My portfolio
+              </NavLink>
             </li>
             <li>
-              <NavLink href="/blog/"><LuNewspaper />My blog</NavLink>
+              <NavLink href="/blog/">
+                <LuNewspaper />
+                My blog
+              </NavLink>
             </li>
             <li>
-              <NavLink href="/contact/"><LuMessageCircle />Contact me</NavLink>
+              <NavLink href="/contact/">
+                <LuMessageCircle />
+                Contact me
+              </NavLink>
             </li>
           </ul>
         </nav>
-        <footer class="content-wrapper">© 2024 - Peter Unger</footer>
+        <footer class={["content-wrapper", css({
+          color: "var(--fg-color-muted)",
+          marginBottom: "17px",
+          marginLeft: "65px",
+          textAlign: "center",
+          md: { 
+            marginLeft: "0",
+          },
+        })]}>© 2024 - Peter Unger</footer>
       </aside>
-      <main>
+      <main
+        class={css({
+          paddingY: "1rem",
+          md: {
+            marginLeft: "var(--sidebar-width)",
+          },
+        })}
+      >
         <div class="container">
           <Slot />
         </div>
@@ -67,3 +155,7 @@ export default component$(() => {
     </>
   );
 });
+function useRef<T>() {
+  throw new Error("Function not implemented.");
+}
+
